6257497c261ca436fef84e6abf4be8de4362c48b
meredithscott131 <scott.mer@northeastern.edu>
2023-07-14 14:03:15 -0400

    Added basic player movement

---
+++ GiraffeSim/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private int speed = 10;
+    private Rigidbody2D characterBody;
+    private Vector2 velocity;
+    private Vector2 inputMovement;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        velocity = new Vector2(speed, speed);
+        characterBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        inputMovement = new Vector2(Input.GetAxisRaw("Horizontal"),
+                                    Input.GetAxisRaw("Vertical"));
+    }
+
+    // Update that happens on a fixed cycle
+    private void FixedUpdate() {
+        Vector2 delta = inputMovement * velocity * Time.deltaTime;
+        Vector2 newPosition = characterBody.position + delta;
+        characterBody.MovePosition(newPosition);
+    }
+}
